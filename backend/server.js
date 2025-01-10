c3d4fe2abad204cde473287b6b1238d6a62f61a5
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-01-10 22:16:12 +0530

    Setting up the mongodb database and node server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB and start server
+connectDB();
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
