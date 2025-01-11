94d59bf680c14b35439194278f76aaf93a241a58
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-01-11 18:47:39 +0530

    Adding source code for authentication validation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,40 +1,72 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Register user
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const existingUser = await User.findOne({ username });
-    if (existingUser) return res.status(400).json({ message: "Username already exists." });
+router.post(
+  "/register",
+  [
+    body("username", "Username must be min 3 length")
+      .isLength({ min: 3 })
+      .exists(),
+    body("password", "Password must be min 8 length.")
+      .isLength({ min: 8 })
+      .exists(),
+  ],
+  async (req, res) => {
+    const { username, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const existingUser = await User.findOne({ username });
+      if (existingUser)
+        return res.status(400).json({ message: "Username already exists." });
 
-    const user = new User({ username, password });
-    await user.save();
+      const user = new User({ username, password });
+      await user.save();
 
-    res.status(201).json({ message: "User registered successfully." });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+      res.status(201).json({ message: "User registered successfully." });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-});
+);
 
 // Login user
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await User.findOne({ username });
-    if (!user) return res.status(400).json({ message: "User not registered" });
+router.post(
+  "/login",
+  [
+    body("username", "Username cannot be blank").exists(),
+    body("password", "Password cannot be blank").exists(),
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid username or password." });
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user)
+        return res.status(400).json({ message: "User not registered" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.status(200).json({ token, username });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ message: "Invalid username or password." });
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.status(200).json({ token, username });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-});
+);
 
 module.exports = router;
