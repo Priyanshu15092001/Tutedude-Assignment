ae7c1b3aac673cf7cb162a8fc0bc144ecd5e1e86
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-01-11 11:51:54 +0530

    Adding source code for sending friend request

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,4 +16,25 @@ router.get("/search", verifyToken, async (req, res) => {
   }
 });
 
+
+// Send friend request
+router.post("/friend-request", verifyToken, async (req, res) => {
+    const { recipientId } = req.body;
+    try {
+      const user = await User.findById(req.userId);
+      const recipient = await User.findById(recipientId);
+  
+      if (!recipient) return res.status(404).json({ message: "User not found." });
+      if (recipient.friendRequests.includes(user._id))
+        return res.status(400).json({ message: "Request already sent." });
+  
+      recipient.friendRequests.push(user._id);
+      await recipient.save();
+  
+      res.status(200).json({ message: "Friend request sent." });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
 module.exports = router;
